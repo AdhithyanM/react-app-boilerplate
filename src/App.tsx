5c50c106311d1695adff7045adf30a4948e703c0
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-11-20 18:01:21 +0530

    material ui setup (Scrapped shadcn)
    Note: clsx, class-variance-authority, tailwind-merge packages are kept as it will be useful in future for developing page components / derived components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import '@styles/globals.css'
+import { Button } from '@mui/material'
 
 const App = (): JSX.Element => {
   return (
     <h1 className="text-red-500 text-4xl font-medium">
       Welcome to React App thats build using Webpack and Babel separately
+      <Button variant="contained" className="text-red-500">
+        Hello world
+      </Button>
     </h1>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,50 @@
-import { createRoot } from 'react-dom/client'
+import { CssBaseline } from '@mui/material'
+import {
+  createTheme,
+  StyledEngineProvider,
+  ThemeProvider,
+} from '@mui/material/styles'
+import '@styles/globals.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store'
 
-const container = document.getElementById('root')
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-if (container === null) {
-  throw new Error('Failed to find the root element')
-}
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement!)
 
-const root = createRoot(container)
+// All `Portal`-related components need to have the the main app wrapper element as a container
+// so that the are in the subtree under the element used in the `important` option of the Tailwind's config.
+const theme = createTheme({
+  components: {
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
+})
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>
 )
